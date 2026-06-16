070a7a61e107aaffb23beddb9117e017f8e93077
Grey Li <withlihui@gmail.com>
2026-06-16 14:02:21 +0800

    初始化Flask项目，添加数据库基础代码

--- app.py
+++ app.py
@@ -1,25 +1,3 @@
-from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy 
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-    # 讲义里的虚拟数据
-    name = 'Grey Li'
-    movies = [
-        {'title': 'My Neighbor Totoro', 'year': '1988'},
-        {'title': 'Dead Poets Society', 'year': '1989'},
-        {'title': 'A Perfect World', 'year': '1993'},
-        {'title': 'Leon', 'year': '1994'},
-        {'title': 'Mojhong', 'year': '1996'},
-        {'title': 'Swallowtail Butterfly', 'year': '1996'},
-        {'title': 'King of Comedy', 'year': '1999'},
-        {'title': 'Devils on the Doorstep', 'year': '1999'},
-        {'title': 'WALL-E', 'year': '2008'},
-        {'title': 'The Pork of Music', 'year': '2012'},
-    ]
-    # render_template(模板文件名, 传给模板的变量)
-    return render_template('index.html', name=name, movies=movies)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+db = SQLAlchemy(app) 
